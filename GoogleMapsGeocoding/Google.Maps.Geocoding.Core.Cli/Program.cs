f1dc2a80d7cbe80154bfd32d43f2bc52ffd263f2
RyanThiele <dynamensions@live.com>
2019-11-02 15:26:41 -0400

    Ported project to .NET core

---
+++ GoogleMapsGeocoding/Google.Maps.Geocoding.Core.Cli/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Google.Maps.Geocoding.Core.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = TestCoder.AllAsync();
+
+            task.Wait();
+
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("Press any key to finish");
+            Console.ReadKey();
+        }
+    }
+}
